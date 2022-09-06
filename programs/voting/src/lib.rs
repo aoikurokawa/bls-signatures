ccfde3387629bae65e230c8da705d1178a7a2d6f
Aoi1011 <ht.so.ik0102@gmail.com>
2022-09-06 05:27:07 -0700

    Update

--- programs/voting/src/lib.rs
+++ programs/voting/src/lib.rs
@@ -3,10 +3,11 @@ use anchor_lang::prelude::*;
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-pub mod my_khe {
+pub mod voting {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        println!("Hello ");
         Ok(())
     }
 }
--- tests/my-khe.ts
+++ tests/my-khe.ts
@@ -14,3 +14,4 @@ describe("my-khe", () => {
     console.log("Your transaction signature", tx);
   });
 });
+``
