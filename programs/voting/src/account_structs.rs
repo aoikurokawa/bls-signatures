650343cc5ca4177e5e50c1ac603e7d1752d8c7c2
Aoi1011 <ht.so.ik0102@gmail.com>
2022-09-07 20:17:27 +0700

    Update

--- programs/voting/src/account_structs.rs
+++ programs/voting/src/account_structs.rs
@@ -1,26 +1,31 @@
 use anchor_lang::prelude::*;
 use crate::state::*;
 
-
-#[derive(Accounts)] 
-pub struct CreatePoll<'info> {
-    #[account(
-        init, 
+#[derive(Accounts)]
+#[instruction(bump: u8)]
+pub struct Initialize<'info> {
+   #[account(
+        init,
+        seeds = [b"initialize_voting".as_ref()],
+        bump,
         payer = payer,
-        space = 8 + 8,
+        space = 8 + PollCount::LEN,
     )]
     pub count_data: Account<'info, PollCount>,
     #[account(mut)]
     pub payer: Signer<'info>,
     pub system_program: Program<'info, System>,
-     
+}
+
+#[derive(Accounts)] 
+pub struct CreatePoll<'info> {
+   #[account(mut)]
+    pub count_data: Account<'info, PollCount>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
 }
 
 #[derive(Accounts)] 
 pub struct VotePoll{
 
 }
-
-
-#[derive(Accounts)]
-pub struct Initialize {}
--- programs/voting/src/lib.rs
+++ programs/voting/src/lib.rs
@@ -4,6 +4,7 @@ mod account_structs;
 mod state;
 
 use account_structs::*;
+use state::*;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -11,9 +12,23 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod voting {
     use super::*;
 
-    pub fn create_poll(ctx: Context<CreatePoll>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, bump: u8) -> Result<()> {
+        let count_data = PollCount {
+            proposal_count: 0,
+            bump,
+        };
+
+        ctx.accounts.count_data.set_inner(count_data);
+
         Ok(())
     }
+
+    pub fn create_poll(ctx: Context<CreatePoll>) -> Result<()> {
+        let count_data = &mut ctx.accounts.count_data;
+
+        Ok(())
+    }
+
     pub fn vote_poll(ctx: Context<VotePoll>) -> Result<()> {
         Ok(())
     }
