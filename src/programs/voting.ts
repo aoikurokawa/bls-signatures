58b7ae9d710ab37c164646bb357e44880084f55f
Aoi1011 <ht.so.ik0102@gmail.com>
2022-09-08 16:48:33 +0700

    Update voting

--- src/programs/voting.ts
+++ src/programs/voting.ts
@@ -1,3 +1,23 @@
-import { AccountMeta } from "@solana/web3.js";
+import type { AccountMeta } from "@solana/web3.js";
+import type { AnchorTypes } from "@saberhq/anchor-contrib";
 
 import type { VotingIDL } from "../idls/voting";
+
+export * from "../idls/voting";
+
+export type VotingType = AnchorTypes<
+  VotingIDL,
+  {
+    pollCountData: PollCountData;
+    pollData: PollData;
+    voteData: VoteData;
+  },
+  {}
+>;
+
+type Accounts = VotingType["Accounts"];
+export type PollCountData = Accounts["PollCount"];
+export type PollData = Accounts["Poll"];
+export type VoteData = Accounts["Vote"];
+
+export type VotingProgram = VotingType["Program"];
