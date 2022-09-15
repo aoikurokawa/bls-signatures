bdaf32042ae1d8762261f36fd69556c9c35a9299
Aoi1011 <ht.so.ik0102@gmail.com>
2022-09-15 14:55:08 +0700

    Change the version of anchor

--- programs/locked-voter/src/lib.rs
+++ programs/locked-voter/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("G8BgM1hwZjPWv8jkJhwpj1WKVneuUUuK9QKXDJxJtX2u");
 
 #[program]
 pub mod locked_voter {
