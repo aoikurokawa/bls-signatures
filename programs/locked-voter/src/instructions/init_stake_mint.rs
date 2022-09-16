e8ebfa6ac512b4af434f64645c36ffcf7d8c4752
Aoi1011 <ht.so.ik0102@gmail.com>
2022-09-16 22:11:44 +0700

    create invoke_signed

--- programs/locked-voter/src/instructions/init_stake_mint.rs
+++ programs/locked-voter/src/instructions/init_stake_mint.rs
@@ -8,8 +8,12 @@ use {
     spl_token,
 };
 
-use anchor_lang::solana_program::program_pack::Pack;
+use anchor_lang::solana_program::{program::invoke_signed, program_pack::Pack};
 use anchor_spl::associated_token;
+use mpl_token_metadata::{
+    instruction::create_metadata_accounts_v2,
+    state::{Creator, Metadata},
+};
 
 use crate::*;
 
@@ -22,7 +26,7 @@ pub struct InitStakeMint<'info> {
 
     pub original_mint: Account<'info, Mint>,
     /// CHECK: This is not dangerous because we don't read or write from this account
-    pub original_mint_metadaat: AccountInfo<'info>,
+    pub original_mint_metadata: AccountInfo<'info>,
 
     #[account(mut)]
     pub vault_mint: Signer<'info>,
@@ -43,9 +47,9 @@ pub struct InitStakeMint<'info> {
     pub system_program: Program<'info, System>,
 }
 
-pub fn handler(ctx: Context<InitStakeMint>) -> Result<()> {
+pub fn handler(ctx: Context<InitStakeMint>, name: String, symbol: String) -> Result<()> {
     let stake_entry = &mut ctx.accounts.stake_entry;
-    let stake_pool_seeds = b"my_khe_stake_pool".as_ref();
+    let stake_pool_seeds = &[b"my_khe_stake_pool".as_ref()];
     let stake_pool_signer = &[&stake_pool_seeds[..]];
     stake_entry.stake_mint = Some(ctx.accounts.vault_mint.key());
 
@@ -80,7 +84,7 @@ pub fn handler(ctx: Context<InitStakeMint>) -> Result<()> {
         Some(&ctx.accounts.stake_pool.key()),
     )?;
 
-    // crate associated token account for stake_entry
+    // create associated token account for stake_entry
     let cpi_accounts = associated_token::Create {
         payer: ctx.accounts.payer.to_account_info(),
         associated_token: ctx.accounts.associated_token.to_account_info(),
@@ -93,6 +97,59 @@ pub fn handler(ctx: Context<InitStakeMint>) -> Result<()> {
     let cpi_program = ctx.accounts.token_program.to_account_info();
     let cpi_context = CpiContext::new(cpi_program, cpi_accounts);
     associated_token::create(cpi_context)?;
+
+    // create metadata
+    let mut metadata_uri_param = "".to_string();
+    if !ctx.accounts.original_mint_metadata.data_is_empty() {
+        let mint_metadata_data = ctx.accounts.original_mint_metadata.try_borrow_data()?;
+        let original_mint_metadata = Metadata::deserialize(&mut mint_metadata_data.as_ref())?;
+        metadata_uri_param = original_mint_metadata
+            .data
+            .uri
+            .trim_matches(char::from(0))
+            .to_string();
+    }
+
+    invoke_signed(
+        &create_metadata_accounts_v2(
+            *ctx.accounts.token_metadata_program.key,
+            *ctx.accounts.vault_mint_metadata.key,
+            *ctx.accounts.vault_mint.key,
+            ctx.accounts.stake_pool.key(),
+            *ctx.accounts.payer.key,
+            ctx.accounts.stake_pool.key(),
+            name,
+            symbol,
+            metadata_uri_param,
+            Some(vec![
+                Creator {
+                    address: ctx.accounts.stake_pool.key(),
+                    verified: true,
+                    share: 50,
+                },
+                Creator {
+                    address: ctx.accounts.stake_entry.key(),
+                    verified: true,
+                    share: 50,
+                },
+            ]),
+            1,
+            true,
+            true,
+            None,
+            None,
+        ),
+        &[
+            ctx.accounts.vault_mint_metadata.to_account_info(),
+            ctx.accounts.vault_mint.to_account_info(),
+            ctx.accounts.stake_pool.to_account_info(),
+            ctx.accounts.payer.to_account_info(),
+            ctx.accounts.system_program.to_account_info(),
+            ctx.accounts.rent.to_account_info(),
+
+        ],
+        stake_pool_signer,
+    )?;
 
     Ok(())
 }
