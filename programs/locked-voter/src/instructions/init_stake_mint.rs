5c215b19dcf1ca7045fcf0cb5e9bb8af6de8845d
Aoi1011 <ht.so.ik0102@gmail.com>
2022-09-16 21:30:23 +0700

    Add spl-token

--- programs/locked-voter/src/instructions/init_stake_mint.rs
+++ programs/locked-voter/src/instructions/init_stake_mint.rs
@@ -1,4 +1,11 @@
-use anchor_spl::{associated_token::AssociatedToken, token::Token};
+use {
+    anchor_lang::solana_program::program::invoke,
+    anchor_lang::solana_program::system_instruction::create_account,
+    anchor_spl::{associated_token::AssociatedToken, token::Token},
+    spl_token,
+};
+
+use anchor_lang::solana_program::program_pack::Pack;
 
 use crate::*;
 
@@ -30,4 +37,31 @@ pub struct InitStakeMint<'info> {
     #[account(address = mpl_token_metadata::id())]
     pub token_metadata_program: UncheckedAccount<'info>,
     pub system_program: Program<'info, System>,
+}
+
+pub fn handler(ctx: Context<InitStakeMint>) -> Result<()> {
+    let stake_entry = &mut ctx.accounts.stake_entry;
+    let stake_pool_seeds = b"my_khe_stake_pool".as_ref();
+    let stake_pool_signer = &[&stake_pool_seeds[..]];
+    stake_entry.stake_mint = Some(ctx.accounts.vault_mint.key());
+
+    // crate mint
+    invoke(
+        &create_account(
+            ctx.accounts.payer.key,
+            ctx.accounts.vault_mint.key,
+            ctx.accounts
+                .rent
+                .minimum_balance(spl_token::state::Mint::LEN),
+            spl_token::state::Mint::LEN as u64,
+            &spl_token::id(),
+        ),
+        &[
+            ctx.accounts.payer.to_account_info(),
+            ctx.accounts.vault_mint.to_account_info(),
+        ],
+    )?;
+
+    // Initialize mint
+    Ok(())
 }
