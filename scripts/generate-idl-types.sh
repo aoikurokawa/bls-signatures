09d029aaa0d24aa9f0501ffb115687cb35cd0b29
Aoi1011 <ht.so.ik0102@gmail.com>
2022-09-21 17:42:22 +0700

    Rewrite script

--- scripts/generate-idl-types.sh
+++ scripts/generate-idl-types.sh
@@ -1,58 +1,15 @@
 #!/usr/bin/env bash
 
-shopt -s extglob
+# This script generates the IDL JSONs without building the full packages.
 
-cd $(dirname $0)/..
+rm -rf src/idl/
+mkdir -p src/idl/
 
-generate_declaration_file() {
-    PROGRAM_SO=$1
-    OUT_DIR=$2
-
-    prog="$(basename $PROGRAM_SO .json)"
-    OUT_PATH="$OUT_DIR/$prog.ts"
-    if [ ! $(which gsed) ]; then
-        PREFIX=$(echo $prog | sed -E 's/(^|_)([a-z])/\U\2/g')
-    else
-        PREFIX=$(echo $prog | gsed -E 's/(^|_)([a-z])/\U\2/g')
-    fi
-    typename="${PREFIX}IDL"
-    rawName="${PREFIX}JSON"
-
-    # types
-    echo "export type $typename =" >>$OUT_PATH
-    cat $PROGRAM_SO >>$OUT_PATH
-    echo ";" >>$OUT_PATH
-
-    # raw json
-    echo "export const $rawName: $typename =" >>$OUT_PATH
-    cat $PROGRAM_SO >>$OUT_PATH
-    echo ";" >>$OUT_PATH
-
-    # error type
-    echo "import { generateErrorMap } from '@saberhq/anchor-contrib';" >>$OUT_PATH
-    echo "export const ${PREFIX}Errors = generateErrorMap($rawName);" >>$OUT_PATH
-}
-
-generate_sdk_idls() {
-    SDK_DIR=${1:-"./packages/sdk/src/idls"}
-    IDL_JSONS=$2
-
-    echo "Generating IDLs for the following programs:"
-    echo $IDL_JSONS
-    echo ""
-
-    rm -rf $SDK_DIR
-    mkdir -p $SDK_DIR
-    if [ $(ls -l artifacts/idl/ | wc -l) -ne 0 ]; then
-        for f in $IDL_JSONS; do
-            generate_declaration_file $f $SDK_DIR
-        done
-        if [[ $RUN_ESLINT != "none" ]]; then
-            yarn eslint --fix $SDK_DIR
-        fi
-    else
-        echo "Warning: no IDLs found. Make sure you ran ./scripts/idl.sh first."
-    fi
-}
-
-generate_sdk_idls ./src/idls/ 'artifacts/idl/*.json'
+for PROGRAM in $(find programs/ -maxdepth 3 -name lib.rs); do
+    PROGRAM_NAME=$(dirname $PROGRAM | xargs dirname | xargs basename | tr '-' '_')
+    echo "Parsing IDL for $PROGRAM_NAME"
+    anchor idl parse --file $PROGRAM --out-ts src/idl/$PROGRAM_NAME.ts || {
+        echo "Could not parse IDL"
+        exit 1
+    }
+done
