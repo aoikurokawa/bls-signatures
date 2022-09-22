ba5f4519d15d75ad8fb74f53da6c17cae5c4a0a7
Aoi1011 <ht.so.ik0102@gmail.com>
2022-09-22 13:37:54 +0700

    Update instruction init poolcount

---
+++ src/programs/voting/instruction.ts
@@ -0,0 +1,35 @@
+import { AnchorProvider, Program } from "@project-serum/anchor";
+import { Wallet } from "@saberhq/solana-contrib";
+import {
+  Connection,
+  Keypair,
+  SystemProgram,
+  TransactionInstruction,
+  PublicKey,
+} from "@solana/web3.js";
+import { VOTING_ADDRESS, VOTING_IDL, VOTING_PROGRAM } from "./constants";
+
+import { findPollCountAddress } from "./pda";
+
+export const initPollCount = (
+  connection: Connection,
+  wallet: Wallet,
+  params: {
+    countDataId: PublicKey;
+  }
+): TransactionInstruction => {
+  const provider = new AnchorProvider(connection, wallet, {});
+  const votingProgram = new Program<VOTING_PROGRAM>(
+    VOTING_IDL,
+    VOTING_ADDRESS,
+    provider
+  );
+
+  return votingProgram.instruction.initialize({
+    accounts: {
+      countData: params.countDataId,
+      payer: wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+  });
+};
